0af872f4d353626c779c6a9dfc44ef6023ba1e68
Friendly-Banana <69007475+Friendly-Banana@users.noreply.github.com>
2023-03-26 18:52:42 +0200

    update config registration

--- fabric/src/main/java/me/banana/no_render/fabric/NoRenderFabric.java
+++ fabric/src/main/java/me/banana/no_render/fabric/NoRenderFabric.java
@@ -1,10 +1,10 @@
 package me.banana.no_render.fabric;
 
+import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
+import fuzs.forgeconfigapiport.api.config.v2.ModConfigEvents;
 import me.banana.no_render.NoRender;
 import me.banana.no_render.NoRenderConfig;
 import net.fabricmc.api.ModInitializer;
-import net.minecraftforge.api.ModLoadingContext;
-import net.minecraftforge.api.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.config.ModConfig;
 
 import static me.banana.no_render.NoRender.MOD_ID;
@@ -13,7 +13,7 @@ public class NoRenderFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         NoRender.init();
-        ModLoadingContext.registerConfig(MOD_ID, ModConfig.Type.CLIENT, NoRenderConfig.GENERAL_SPEC);
-        ModConfigEvent.RELOADING.register(NoRender::checkConfigReload);
+        ForgeConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.CLIENT, NoRenderConfig.GENERAL_SPEC);
+        ModConfigEvents.reloading(MOD_ID).register(NoRender::checkConfigReload);
     }
 }
